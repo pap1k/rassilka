5b70dd94c1d98b9dd538e1e4e33931b0551adc73
pap1k <magemaster555@gmail.com>
2024-10-23 23:01:57 +0300

    suka proverka

--- autosend.py
+++ autosend.py
@@ -54,6 +54,7 @@ async def sender(distrib: Distribs, u: User, delay: float):
         @app.on(events.NewMessage)
         async def new_handler(event):
             if event.message.mentioned:
+                print(event.message.peer_id)
                 peer_id = int(event.message.peer_id) 
                 if peer_id in resendcounter:
                     resendcounter[peer_id] += 1
